52e78f4a88c2389bc8619632d58a486e09ea2c06
Bob <tesakb903@gmail.com>
2020-12-12 19:07:45 +0200

    modal

--- js/style.js
+++ js/style.js
@@ -1,7 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu_left'),
         menuItem = document.querySelectorAll('.menu_item'),
-        hamburger = document.querySelector('.burger');
+        hamburger = document.querySelector('.burger'),
+        modal = document.querySelector(".mod"),
+        close = document.querySelector(".mod_close"),
+        but = document.querySelector(".menu_link-btn");
+
+    console.log(modal);
+    console.log(close);
+    console.log(but);
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('burger_active');
@@ -13,5 +20,15 @@ window.addEventListener('DOMContentLoaded', () => {
             hamburger.classList.toggle('burger_active');
             menu.classList.toggle('menu_left_active');
         });
+    });
+
+    but.addEventListener("click", () => {
+        modal.classList.add("mod_show");
+        modal.classList.remove("mod_hide");
+    });
+
+    close.addEventListener("click", () => {
+        modal.classList.add("mod_hide");
+        modal.classList.remove("mod_show");
     });
 });
